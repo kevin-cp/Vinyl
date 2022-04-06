8177de63e3df38bc2e7bcc39262f3ae3b397b1b1
kevin-cp <kevinpeixoto182@gmail.com>
2022-04-06 14:57:28 +0200

    password modifier WIP

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +16,11 @@ class AccountPasswordController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('account/password.html.twig');
+
+        $form = $this->createForm(PasswordType::class);
+        $viewForm = $form->createView();
+
+        return $this->render('account/password.html.twig', compact('viewForm'));
     }
 }
  
